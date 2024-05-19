489a897c8f417105a6b438f59db755ac9fd6a2ff
Mathias Prince Lawson <73763892+ingucell@users.noreply.github.com>
2024-05-19 10:54:20 +0000

    hover effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,90 +1,128 @@
-import React from 'react'
+import React, { useContext } from "react";
 
-
+import { HoverContext } from "../contexts/HoverContext";
 import { CiClock2 } from "react-icons/ci";
 import { TfiEmail } from "react-icons/tfi";
-import { FaPhoneAlt, FaInstagram, FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import {
+  FaPhoneAlt,
+  FaInstagram,
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            <nav className='w-full'>
-                <div className='flex justify-between w-full h-[100px] bg-black text-white items-center'>
-                    <h1 className='text-2xl font-bold ml-[40px]'>Coronation</h1>
-                    <div className='w-[1199px] h-[68px] flex gap-5 items-center justify-evenly'>
-                        <div className='flex items-center gap-4'>
-                            <div className='rounded-full border border-1 border-customPurple bg-transparent flex p-4 w-[63px] h-[63px] items-center justify-center'>
-                                <CiClock2 className='text-customPurple' />
-                            </div>
-                            <div className='flex flex-col'>
-                                <small>Monday-Friday</small>
-                                <small>Saturday-Sunday Closed</small>
-                            </div>
-                        </div>
-                        <div className='flex items-center gap-6'>
-                            <div className='rounded-full border border-1 border-customPurple bg-transparent flex p-4 w-[63px] h-[63px] items-center justify-center'>
-                                <TfiEmail className='text-customPurple' />
-                            </div>
-                            <div className='flex flex-col'>
-                                <small>Email</small>
-                                <small>contact@coronation.com</small>
-                            </div>
-                        </div>
-                        <div className='flex items-center gap-6'>
-                            <div className='rounded-full border border-1 border-customPurple bg-transparent flex p-4 w-[63px] h-[63px] items-center justify-center'>
-                                <FaPhoneAlt className='text-customPurple' />
-                            </div>
-                            <div className='flex flex-col'>
-                                <small>Call us</small>
-                                <small>(00) 112 365 489</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className={`navbar w-full h-[78px] flex items-center justify-center absolute top-[100px] z-50 bg-black opacity-50`}>
-                    <div className='w-full h-full flex items-center justify-between px-[200px]'>
-                        <ul className='flex justify-evenly text-white w-[700px]'>
-                            <li className='border-r p-1 flex'>
-                                <Link to="/about">ABOUT</Link>
-                            </li>
-                            <li className='border-r p-1 flex'>
-                                <Link to="/products">PRODUCTS & SOLUTIONS</Link>
-                            </li>
-                            <li className='border-r p-1 flex'>
-                                <Link to="/insights">INSIGHTS</Link>
-                            </li>
-                            <li className='border-r p-1 flex'>
-                                <Link to="/careers">CAREERS</Link>
-                            </li>
-                            <li className='p-1 flex'>
-                                <Link to="/contact">CONTACT US</Link>
-                            </li>
-                        </ul>
-                        <ul className='flex gap-4 text-white items-center'>
-                            <li>
-                                <Link to="#"><FaInstagram /></Link>
-                            </li>
-                            <li>
-                                <Link to="#"><FaFacebookF /></Link>
-                            </li>
-                            <li>
-                                <Link to="#"><FaTwitter /></Link>
-                            </li>
-                            <li>
-                                <Link to="#"><FaLinkedinIn /></Link>
-                            </li>
-                            <li className='bg-white text-black px-4 py-2 rounded'>
-                                <Link to="/">Self Services</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+  const { isHovered } = useContext(HoverContext);
 
+  //   ${isHovered ? 'bg-blue-500' : 'bg-red-500'}
 
+  return (
+    <div>
+      <nav className="w-full">
+        <div
+          className={`flex justify-between w-full h-[100px] text-white items-center  ${
+            isHovered === "left" ? "bg-primary text-black" : "bg-secondary"
+          }`}
+        >
+          <h1
+            className={`${
+              isHovered === "left" ? "text-customPurple" : "text-white"
+            } text-2xl font-bold ml-[40px]`}
+          >
+            Coronation
+          </h1>
+          <div
+            className={`w-[1199px] h-[68px] flex gap-5 items-center justify-evenly ${
+              isHovered === "left" ? "text-customPurple" : "text-white"
+            }`}
+          >
+            <div className="flex items-center gap-4">
+              <div className="rounded-full border border-1 border-customPurple bg-transparent flex p-4 w-[63px] h-[63px] items-center justify-center">
+                <CiClock2 className="text-customPurple" />
+              </div>
+              <div className="flex flex-col">
+                <small>Monday-Friday</small>
+                <small>Saturday-Sunday Closed</small>
+              </div>
+            </div>
+            <div className="flex items-center gap-6">
+              <div className="rounded-full border border-1 border-customPurple bg-transparent flex p-4 w-[63px] h-[63px] items-center justify-center">
+                <TfiEmail className="text-customPurple" />
+              </div>
+              <div className={`flex flex-col`}>
+                <small>Email</small>
+                <small>contact@coronation.com</small>
+              </div>
+            </div>
+            <div className="flex items-center gap-6">
+              <div className="rounded-full border border-1 border-customPurple bg-transparent flex p-4 w-[63px] h-[63px] items-center justify-center">
+                <FaPhoneAlt className="text-customPurple" />
+              </div>
+              <div className="flex flex-col">
+                <small>Call us</small>
+                <small>(00) 112 365 489</small>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={`navbar w-full h-[78px] flex items-center justify-center absolute top-[100px] z-50  ${
+                isHovered === "left" ? "bg-primary text-black text-customPurple" : "bg-secondary"
+              }  opacity-90`}
+          >
+            <div className="w-full h-full flex items-center justify-between px-[200px]">
+              {/* left side */}
+              <ul className="flex justify-evenly text-white w-[700px]">
+                <li className="border-r p-1 flex">
+                  <Link to="/about">ABOUT</Link>
+                </li>
+                <li className="border-r p-1 flex">
+                  <Link to="/products">PRODUCTS & SOLUTIONS</Link>
+                </li>
+                <li className="border-r p-1 flex">
+                  <Link to="/insights">INSIGHTS</Link>
+                </li>
+                <li className="border-r p-1 flex">
+                  <Link to="/careers">CAREERS</Link>
+                </li>
+                <li className="p-1 flex">
+                  <Link to="/contact">CONTACT US</Link>
+                </li>
+              </ul>
+
+              {/* right side */}
+              <ul className={`flex gap-4 text-white items-center  `}>
+                <li>
+                  <Link to="#">
+                    <FaInstagram />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <FaFacebookF />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <FaTwitter />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="#">
+                    <FaLinkedinIn />
+                  </Link>
+                </li>
+                <li className="bg-white text-black px-4 py-2 rounded">
+                  <Link to="/">Self Services</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-    );
-}
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,83 @@
-import React, { useState } from 'react';
-import landInd from '../assets/landingInd.png';
+import React, { useState, useContext } from "react";
+import landInd from "../assets/landingInd.png";
 import landCop from "../assets/landingCop.png";
 import landingImg from "../assets/landingImg.png";
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
+import { HoverContext } from "../contexts/HoverContext";
 
 const Home = () => {
-    const [hovered, setHovered] = useState(false);
+  const [hovered, setHovered] = useState(false);
 
-    const handleHover = (side) => {
-        setHovered(side);
-    };
+  const handleHover = (side) => {
+    setHovered(side);
+  };
 
-    return (
-        <div>
-            <div className='relative flex'
-                onMouseEnter={() => handleHover('left')}
-                onMouseLeave={() => handleHover(false)}
-            >
-                <div className='relative w-full h-screen'>
-                    <img src={landInd} alt='individual' className='absolute inset-0 w-full h-full object-cover' />
+  const { setIsHovered } = useContext(HoverContext);
 
-                    <div cclassName={`absolute w-[335px] h-[316px] flex flex-col justify-center items-start top-[200px] left-[50px] text-white z-50`}>
-                        <h1 className='text-4xl font-semibold mb-4'>INDIVIDUAL</h1>
-                        <p className='mb-6 text-[24px] leading-[38px]'>Invest and manage your money on your own terms. It's easier than ever before with Coronation Insurance.</p>
-                        <button className='bg-primary text-white font-bold py-2 px-4 rounded w-[212px]'>Enter</button>
-                    </div>
+  return (
+    <div>
+      <div
+        className="relative flex"
+      >
+        {/* left side */}
+        <div
+          className="relative w-full h-screen"
+          onMouseEnter={() => setIsHovered("left")}
+          onMouseLeave={() => setIsHovered("left")}
+        >
+          <img
+            src={landInd}
+            alt="individual"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
 
-                    <div className='absolute inset-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 hover:opacity-90 z-60'></div>
-                </div>
+          <div
+            cclassName={`absolute w-[335px] h-[316px] flex flex-col justify-center items-start top-[200px] left-[50px] text-white z-50`}
+          >
+            <h1 className="text-4xl font-semibold mb-4">INDIVIDUAL</h1>
+            <p className="mb-6 text-[24px] leading-[38px]">
+              Invest and manage your money on your own terms. It's easier than
+              ever before with Coronation Insurance.
+            </p>
+            <button className="bg-primary text-white font-bold py-2 px-4 rounded w-[212px]">
+              Enter
+            </button>
+          </div>
 
-
-                <div className='relative w-full h-screen'>
-                    <img src={landCop} alt='corporate' className='absolute inset-0 w-full h-full object-cover' />
-                    <div className='absolute w-[335px] h-[316px] flex flex-col justify-end items-end top-[200px] right-[50px] text-white'>
-                        <h1 className='text-4xl font-semibold mb-4'>CORPORATE</h1>
-                        <p className='mb-6 text-[24px] leading-[38px] text-right'>Invest and manage your money on your own terms. It's easier than ever before with Coronation Insurance.</p>
-                        <button className='bg-secondary text-white font-bold py-2 px-4 rounded w-[212px]'>Enter</button>
-                    </div>
-                </div>
-
-                <img src={landingImg} alt='landing' className='absolute h-[600px] top-[80px] left-[752px] transform -translate-x-1/2 z-30' />
-            </div>
+          <div className="absolute inset-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 z-60"></div>
         </div>
-    );
-}
+
+        {/* right side */}
+        <div
+          className="relative w-full h-screen"
+          onMouseEnter={() => setIsHovered("right")}
+          onMouseLeave={() => setIsHovered("right")}
+        >
+          <img
+            src={landCop}
+            alt="corporate"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+          <div className="absolute w-[335px] h-[316px] flex flex-col justify-end items-end top-[200px] right-[50px] text-white">
+            <h1 className="text-4xl font-semibold mb-4">CORPORATE</h1>
+            <p className="mb-6 text-[24px] leading-[38px] text-right">
+              Invest and manage your money on your own terms. It's easier than
+              ever before with Coronation Insurance.
+            </p>
+            <button className="bg-secondary text-white font-bold py-2 px-4 rounded w-[212px]">
+              Enter
+            </button>
+          </div>
+        </div>
+
+        <img
+          src={landingImg}
+          alt="landing"
+          className="absolute h-[600px] top-[80px] left-[752px] transform -translate-x-1/2 z-30"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
